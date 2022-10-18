d94bf761a10af6fea03f93c06d2a2cfa0b4659cf
Joey Hoellerich <joeyhoellerich@gmail.com>
2022-10-18 13:13:04 -0400

    added state tracking to login and sign up forms + general styling changes

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../imgs/whereRU_logo.svg"
 
 function Login(){
+
+    let [currentUser, setCurrentUser] = useState({
+        username: "",
+        password: ""
+    })
+
+    let [invalidCombo, setInvalidCombo] = useState(false)
+
+    let updateForm = (value) => {
+        if (invalidCombo && value === {password: ""}){
+            setInvalidCombo(false)
+        }
+        return setCurrentUser((prev) => {
+            return {...prev, ...value}
+        })
+    }
+
+    let handleSubmit = async (e) => {
+        e.preventDefault()
+        setInvalidCombo(true)
+        console.log(currentUser)
+    }
 
     return(
         <div className="loginContainer">
@@ -10,13 +32,26 @@ function Login(){
                 <form>
                     <label htmlFor="username">Username: </label>
                     <br/>
-                    <input type="text" name="username"></input> 
+                    <input 
+                        type="text" 
+                        name="username"
+                        value = {currentUser.username}
+                        onChange = {(e) => updateForm({username: e.target.value})}
+                        >
+                    </input> 
                     <br/>
                     <label htmlFor="password">Password: </label>
                     <br/>
-                    <input type="password" name="password"></input> 
+                    <input 
+                        type="password" 
+                        name="password"
+                        value = {currentUser.password}
+                        onChange = {e => updateForm({password: e.target.value})}
+                        >
+                    </input> 
                     <br/>
-                    <button type="submit">Log In</button>
+                    <button type="submit" onClick={handleSubmit}>Log In</button>
+                    {invalidCombo ? <p>Invalid Username or Password. Please try again, or make an account</p> : <></>}
                 </form>
             </div>
             <img  src = {logo} alt="logo"/>
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../imgs/whereRU_logo.svg"
 
 function SignUp(){
+
+    let [currentUser, setCurrentUser] = useState({
+        username: "",
+        password: ""
+    })
+
+    let [invalidName, setInvalidName] = useState(false)
+
+    let updateForm = (value) => {
+        return setCurrentUser((prev) => {
+            return {...prev, ...value}
+        })
+    }
+
+    let handleSubmit = async (e) => {
+        e.preventDefault()
+        if (currentUser.username === "" || currentUser.password === ""){
+            setInvalidName(true)
+        }
+        console.log(currentUser)
+    }
 
     return(
         <div className="loginContainer">
@@ -10,14 +31,31 @@ function SignUp(){
                 <form>
                     <label htmlFor="username">Username: </label>
                     <br/>
-                    <input type="text" name="username"></input> 
+                    <input
+                        type="text" 
+                        name="username"
+                        value = {currentUser.username}
+                        onChange = {e => updateForm({username: e.target.value})}
+                        required = {true}
+                        minLength={5}
+                        >
+                    </input> 
                     <br/>
                     <label htmlFor="password">Password: </label>
                     <br/>
-                    <input type="password" name="password"></input> 
+                    <input
+                        type="password" 
+                        name="password"
+                        value = {currentUser.password}
+                        onChange = {e => updateForm({password: e.target.value})}
+                        required = {true}
+                        minLength={5}
+                        >
+                    </input> 
                     <br/>
-                    <button type="submit">Create Account</button>
+                    <button type="submit" onClick={handleSubmit}>Create Account</button>
                 </form>
+                {invalidName ? <p>This username has already been taken</p> : <></>}
             </div>
             <img  src = {logo} alt="logo"/>
 
