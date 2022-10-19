5481374363969e142f768241033c929b5e5af94e
Joey Hoellerich <joeyhoellerich@gmail.com>
2022-10-19 18:59:30 -0400

    jwt authentication backend

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,7 @@
 const users = require('express').Router();
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 users.get('/:username', async (req, res) => {
     let username = req.params.username
@@ -27,6 +28,45 @@ users.post('/', async (req, res) => {
         res.json(user)
     } else {
         res.send(false)
+    }
+})
+
+users.post('/authentication', async (req, res) => {
+    let user = User.findOne({
+        where: {username: req.body.username}
+    })
+
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({
+            message: "incorrect username or password"
+        })
+    }
+
+    else {
+        const token = jwt.sign({username: user.username}, process.env.JWT_SECRET)
+        res.status(200).json({user: user, token: token})
+    }
+})
+
+users.get("/authentication/profile", async (req, res) => {
+    try {
+        //Split authorization header into ["Bearer", "Token"]
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        if (authenticationMethod == "Bearer"){
+            const result = jwt.verify(token, process.env.JWT_SECRET)
+
+            const { username } = result
+            
+            let user = await User.findOne({
+                where: {
+                    username: username
+                }
+            })
+            res.json(user)
+        }
+    } catch {
+        res.json(null)
     }
 })
 
